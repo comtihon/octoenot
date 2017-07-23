471a57a1eeb883564abaab84f0bf9467c512927a
Valerii Tikhonov <com.tihon@mail.ru>
2017-07-23 12:50:29 +0200

    change body fetching

--- src/handler/oc_callback_handler.erl
+++ src/handler/oc_callback_handler.erl
@@ -16,7 +16,7 @@
 
 init(Req0, State) ->
   AllHeaders = cowboy_req:headers(Req0),
-  {ok, Data, Req} = cowboy_req:read_urlencoded_body(Req0),
+  {ok, Data, Req} = cowboy_req:read_body(Req0),
   Req1 = act_callback(AllHeaders, Data, Req),
   {ok, Req1, State}.
 
@@ -24,9 +24,8 @@ init(Req0, State) ->
 %% @private
 act_callback(Headers = #{<<"x-github-event">> := <<"create">>}, Body, Req0) ->
   oc_logger:info("Headers ~p", [Headers]),
-  Payload = proplists:get_value(<<"payload">>, Body),
-  Decoded = jsone:decode(Payload, [{object_format, map}]),
-  try oc_loader_mngr:add_package(Decoded) of
+  Decoded = jsone:decode(Body, [{object_format, map}]),
+  try oc_loader_mngr:add_package(select_package(Decoded)) of
     true ->
       cowboy_req:reply(200, #{<<"content-type">> => <<"text/plain">>}, <<"OK">>, Req0)
   catch
@@ -38,3 +37,7 @@ act_callback(#{<<"accept">> := _}, _Body, Req0) ->
 act_callback(Headers, _Body, Req0) ->
   oc_logger:warn("Unknown callback: ~p", [Headers]),
   cowboy_req:reply(400, #{<<"content-type">> => <<"text/plain">>}, ?UNSUPPORTED_TYPE, Req0).
+
+%% @private
+select_package(#{<<"payload">> := Package}) -> Package;  % in webhook data is wrapped in a payload
+select_package(Package) -> Package.  % in github app data is plain
